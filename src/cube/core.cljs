dd89bac58974973ed21f84d31057cd05f5e398b2
Shaun Williams <swilliams@pros.com>
2014-07-05 00:49:20 -0500

    add doc strings and make search function return path

--- src/cube/core.cljs
+++ src/cube/core.cljs
@@ -9,21 +9,17 @@
                 [ 1  0  0] "right"
                 [-1  0  0] "left"})
 
+(def dirs (keys dir-names))
+
 (def initial-segs [2 2 2 2 1 1 1 2 2 1 1 2 1 2 1 1 2])
 (def initial-pos [0 0 0])
 (def initial-dir [0 0 1])
 (def initial-cube #{initial-pos})
 
-(def dirs [[ 0  0  1]
-           [ 0  0 -1]
-           [ 0  1  0]
-           [ 0 -1  0]
-           [ 1  0  0]
-           [-1  0  0]])
-
 (defn log [x] (js/console.log (pr-str x)))
 
 (defn write-seg
+  "Get a new cube with the given segment filled, or nil if not possible."
   [[x y z] [dx dy dz] len cube]
   (let [new-pos [(+ x dx) (+ y dy) (+ z dz)]
         new-cube (conj cube new-pos)]
@@ -33,17 +29,20 @@
     :else (recur new-pos [dx dy dz] (dec len) new-cube))))
 
 (defn dim-width
+  "Get the dimension width of the given axis."
   [cube i]
   (let [xs (map #(nth % i) cube)]
     (js/Math.abs (- (apply min xs) (apply max xs)))))
 
 (defn valid-cube?
+  "Determine if the given potential cube is within valid boundaries."
   [cube]
   (and (<= (dim-width cube 0) 2)
        (<= (dim-width cube 1) 2)
        (<= (dim-width cube 2) 2)))
 
 (defn get-dir-axis
+  "Get the axis index of the given direction vector."
   [dir]
   (->> dir
        (map-indexed vector)
@@ -52,32 +51,26 @@
        (first)))
 
 (defn get-dir-options
+  "Get the potential next directions from the current direction (perpendicular)."
   [dir]
   (let [i (get-dir-axis dir)]
     (filter #(zero? (nth % i)) dirs)))
 
-(defn get-valid-dirs
-  [pos dirs cube]
-  )
-
 (defn search
+  "Get a path to fill the given cube with the given segments, or nil if impossible."
   [pos dir cube segs]
   (if-not (seq segs)
-    (do
-      (log "done.")
-      (log cube)
-      (log (count cube))
-      true)
-    (if-let [new-cube (write-seg pos dir (first segs) cube)]
+    []
+    (when-let [new-cube (write-seg pos dir (first segs) cube)]
       (when (valid-cube? new-cube)
         (let [new-pos (mapv (fn [a da] (+ a (* da (first segs)))) pos dir)
               dirs (get-dir-options dir)
               search-next #(search new-pos % new-cube (rest segs))]
-          (when-let [success-path (first (filter search-next dirs))]
-            (log (get dir-names dir))
-            true))))))
+          (when-let [success-path (some search-next dirs)]
+            (conj success-path (get dir-names dir))))))))
 
-(log (search initial-pos
-             initial-dir
-             initial-cube
-             initial-segs))
+
+(log (reverse (search initial-pos
+                      initial-dir
+                      initial-cube
+                      initial-segs)))
